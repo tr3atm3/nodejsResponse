aa79c6f56953ed6b2c8efa33d9039e7fde672bf7
Tushar Sharma <tttushar.789@gmail.com>
2024-08-17 23:38:05 +0530

    Express

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes.handler);
-server.listen(4000);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from express </h1>");
+});
+
+app.listen(3000);
